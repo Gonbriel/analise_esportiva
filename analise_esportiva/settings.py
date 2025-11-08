8a780d1e199d318757f1235a543d57770021d2a5
Gonbriel <gonbriel03@gmail.com>
2025-11-08 16:15:43 -0300

    Atualizando banco para postgre

--- analise_esportiva/settings.py
+++ analise_esportiva/settings.py
@@ -66,11 +66,12 @@ WSGI_APPLICATION = 'analise_esportiva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
